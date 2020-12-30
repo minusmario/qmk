fc1641e1d715b103a856f9be789f9c1421a78741
MinusMario <tzzhmagnifica@gmail.com>
2020-12-30 15:07:06 +0800

    - add two tone to song_list.h - minusmario's preonic keymap

---
+++ keyboards/preonic/keymaps/minusmario/config.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+    #define STARTUP_SONG SONG(PREONIC_SOUND)
+    #define GOODBYE_SONG SONG(GOODBYE_SOUND)
+    // #define STARTUP_SONG SONG(NO_SOUND)
+
+    /*#define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
+                                  SONG(COLEMAK_SOUND), \
+                                  SONG(DVORAK_SOUND) \
+                                }*/
+#endif
+
+//#define MUSIC_MASK (keycode != KC_NO)
+
+/*
+ * MIDI options
+ */
+
+/* Prevent use of disabled MIDI features in the keymap */
+//#define MIDI_ENABLE_STRICT 1
+
+/* enable basic MIDI features:
+   - MIDI notes can be sent when in Music mode is on
+*/
+
+// #define MIDI_BASIC
+
+/* enable advanced MIDI features:
+   - MIDI notes can be added to the keymap
+   - Octave shift and transpose
+   - Virtual sustain, portamento, and modulation wheel
+   - etc.
+*/
+//#define MIDI_ADVANCED
+
+/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
+//#define MIDI_TONE_KEYCODE_OCTAVES 2
+#undef DEBOUNCE
+#define DEBOUNCE 5
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 150
+#define PERMISSIVE_HOLD
+
+#undef ONESHOT_TIMEOUT
+#define ONESHOT_TIMEOUT 3000
+#define LEADER_TIMEOUT 500
+#define LEADER_PER_KEY_TIMING
+#define USB_POLLING_INTERVAL_MS 1
---
+++ keyboards/preonic/keymaps/minusmario/rules.mk
@@ -0,0 +1,8 @@
+SRC += muse.c #音乐
+DYNAMIC_MACRO_ENABLE = yes # 动态宏
+LEADER_ENABLE = yes # leader key
+RGBLIGHT_ENABLE = yes # RGB底灯
+BOOTMAGIC_ENABLE = full # boot magic
+EXTRAKEY_ENABLE = yes # 媒体按键
+NKRO_ENABLE = yes # 全键无冲
+LAYOUTS = LAYOUT_preonic_2x2u # 双空格layout
