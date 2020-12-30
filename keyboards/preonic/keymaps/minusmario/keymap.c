74c9fc4bf50c09f9c0265d78d40813e1a201520f
MinusMario <tzzhmagnifica@gmail.com>
2020-12-30 17:57:58 +0800

    code lint

--- keyboards/preonic/keymaps/minusmario/keymap.c
+++ keyboards/preonic/keymaps/minusmario/keymap.c
@@ -54,19 +54,19 @@ float audio_game_off[][2] = SONG(GAME_OFF);
 #endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_LOWER] = LAYOUT_preonic_2x2u(
-KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,       KC_F7,      KC_F8,          KC_F9, KC_F10, KC_F11,            KC_F12,           \
-RSFT(KC_GRAVE), RSFT(KC_1),     RSFT(KC_2),     RSFT(KC_3),     RSFT(KC_4),     RSFT(KC_5),  LSFT(KC_6), KC_7,           KC_8,  KC_9,   LSFT(KC_9),        LSFT(KC_0),       \
-KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,       KC_F6,      KC_4,           KC_5,  KC_6,   KC_LBRACKET,       KC_RBRACKET,      \
-KC_TRANSPARENT, KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,      KC_F12,     KC_1,           KC_2,  KC_3,   LSFT(KC_LBRACKET), LSFT(KC_RBRACKET),\
-KC_NO,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,        KC_TRANSPARENT,              KC_TRANSPARENT,      KC_0,  KC_DOT, KC_PLUS,           KC_MINUS
-),
 [_DEFAULT] = LAYOUT_preonic_2x2u(
-KC_GRV,         KC_1,     KC_2,    KC_3,    KC_4,        KC_5,  KC_6,         KC_7,  KC_8,     KC_9,    KC_0,      KC_CAPS,       \
-KC_ESCAPE,      KC_Q,     KC_W,    KC_E,    KC_R,        KC_T,  KC_Y,         KC_U,  KC_I,     KC_O,    KC_P,      KC_BSPACE,       \
-LCTL_T(KC_TAB), KC_A,     KC_S,    KC_D,    KC_F,        KC_G,  KC_H,         KC_J,  KC_K,     KC_L,    KC_SCOLON, KC_QUOTE,        \
-KC_LSHIFT,      KC_Z,     KC_X,    KC_C,    KC_V,        KC_B,  KC_N,         KC_M,  KC_COMMA, KC_DOT,  KC_SLASH,  RSFT_T(KC_ENTER),\
-FN,             KC_LCTRL, KC_LGUI, KC_LALT, LT(_LOWER, KC_SPC), LT(_RAISE,KC_SPC),   KC_LEFT,  KC_DOWN, KC_UP,     KC_RIGHT
+KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,        KC_5,       KC_6,       KC_7,  KC_8,     KC_9,    KC_0,              KC_CAPS,          \
+KC_ESCAPE,      KC_Q,           KC_W,           KC_E,           KC_R,        KC_T,       KC_Y,       KC_U,  KC_I,     KC_O,    KC_P,              KC_BSPACE,        \
+LCTL_T(KC_TAB), KC_A,           KC_S,           KC_D,           KC_F,        KC_G,       KC_H,       KC_J,  KC_K,     KC_L,    KC_SCOLON,         KC_QUOTE,         \
+KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,        KC_B,       KC_N,       KC_M,  KC_COMMA, KC_DOT,  KC_SLASH,          RSFT_T(KC_ENTER), \
+FN,             KC_LCTRL,       KC_LGUI,        KC_LALT,             LT(_LOWER, KC_SPC), LT(_RAISE,KC_SPC), KC_LEFT,  KC_DOWN, KC_UP,             KC_RIGHT
+),
+[_LOWER] = LAYOUT_preonic_2x2u(
+KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,       KC_F6,      KC_F7,      KC_F8, KC_F9,    KC_F10,  KC_F11,            KC_F12,           \
+RSFT(KC_GRAVE), RSFT(KC_1),     RSFT(KC_2),     RSFT(KC_3),     RSFT(KC_4),  RSFT(KC_5), LSFT(KC_6), KC_7,  KC_8,     KC_9,    LSFT(KC_9),        LSFT(KC_0),       \
+KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,       KC_F5,      KC_F6,      KC_4,  KC_5,     KC_6,    KC_LBRACKET,       KC_RBRACKET,      \
+KC_TRANSPARENT, KC_F7,          KC_F8,          KC_F9,          KC_F10,      KC_F11,     KC_F12,     KC_1,  KC_2,     KC_3,    LSFT(KC_LBRACKET), LSFT(KC_RBRACKET),\
+KC_NO,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,      KC_TRANSPARENT,     KC_TRANSPARENT,    KC_0,     KC_DOT,  KC_PLUS,           KC_MINUS
 ),
 [_RAISE] = LAYOUT_preonic_2x2u(
 KC_F1,          KC_F2,           KC_F3,           KC_F4,          KC_F5,          KC_F6,       KC_F7,     KC_F8,          KC_F9,               KC_F10,               KC_F11,              KC_F12,          \
@@ -87,7 +87,7 @@ KC_NO,         KC_NO,   KC_NO, KC_NO,  KC_NO,  KC_NO,   KC_NO,             KC_NO
 KC_DOT,        KC_1,    KC_2,  KC_3,   KC_4,   KC_5,    KC_PLUS,           KC_MINUS,KC_ASTERISK,KC_SLASH,      KC_LPRN,KC_RPRN,\
 KC_COMMA,      KC_6,    KC_7,  KC_8,   KC_9,   KC_0,    MARCO_VIM_PREVIOUS,KC_NO,   KC_NO,      MARCO_VIM_NEXT,KC_NO,  KC_NO,  \
 KC_SCOLON,     KC_QUOTE,KC_DQT,KC_LABK,KC_RABK,KC_NO,   KC_NO,             KC_NO,   KC_NO,      KC_NO,         KC_NO,  KC_NO,  \
-KC_TRANSPARENT,KC_NO,   KC_NO, KC_NO,     KC_NO,                KC_SPC,             KC_NO,      KC_NO,         KC_NO,  KC_NO
+KC_TRANSPARENT,KC_NO,   KC_NO, KC_NO,       KC_NO,                KC_SPC,           KC_NO,      KC_NO,         KC_NO,  KC_NO
 ),
 [_GAME] = LAYOUT_preonic_2x2u(
 KC_ESCAPE, KC_1,     KC_2,    KC_3,    KC_4,        KC_5,  KC_6,         KC_7,  KC_8,     KC_9,    KC_0,      DEFA,            \
