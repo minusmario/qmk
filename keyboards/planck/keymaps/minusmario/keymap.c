7656fefd449cafe7748bd5358b0ea6533f2c6120
MinusMario <tzzhmagnifica@gmail.com>
2021-02-06 15:44:51 +0800

    copy planck keymap from old repo

--- keyboards/planck/keymaps/minusmario/keymap.c
+++ keyboards/planck/keymaps/minusmario/keymap.c
@@ -30,44 +30,57 @@ enum planck_layers {
   _LOWER,
   _RAISE,
   _ADJUST,
+  _FN
 };
 
 enum planck_keycodes {
-  ST_MACRO_0 = SAFE_RANGE,
-  ST_MACRO_1,
-  ST_MACRO_2,
-  ST_MACRO_3,
+  MARCO_VIM_NEXT = SAFE_RANGE,
+  MARCO_VIM_PREVIOUS,
+  MARCO_VIM_YANK,
+  MARCO_VIM_PASTE,
+  MARCO_VIM_FIND,
   ALT_TAB
 };
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
+#define FN MO(_FN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_BASE] = LAYOUT_planck_mit(
+  [_BASE] = LAYOUT_planck_mit(
     KC_ESCAPE,      KC_Q,     KC_W,    KC_E,    KC_R,  KC_T,     KC_Y, KC_U,  KC_I,     KC_O,    KC_P,      KC_BSPACE,     \
     LCTL_T(KC_TAB), KC_A,     KC_S,    KC_D,    KC_F,  KC_G,     KC_H, KC_J,  KC_K,     KC_L,    KC_SCOLON, KC_QUOTE,     \
     KC_LSHIFT,      KC_Z,     KC_X,    KC_C,    KC_V,  KC_B,     KC_N, KC_M,  KC_COMMA, KC_DOT,  KC_SLASH,  RSFT_T(KC_ENTER), \
-    KC_LEAD,        KC_LCTRL, KC_LGUI, KC_LALT, LOWER, KC_SPACE,       RAISE, KC_LEFT,  KC_DOWN, KC_UP,     KC_RIGHT
-),
-[_LOWER] = LAYOUT_planck_mit(
+    FN,             KC_LCTRL, KC_LGUI, KC_LALT, LOWER, KC_SPACE,       RAISE, KC_LEFT,  KC_DOWN, KC_UP,     KC_RIGHT
+  ),
+
+  [_LOWER] = LAYOUT_planck_mit(
     RSFT(KC_GRAVE), RSFT(KC_1),     RSFT(KC_2),     RSFT(KC_3),     RSFT(KC_4),     RSFT(KC_5),  LSFT(KC_6), KC_7,           KC_8, KC_9,   LSFT(KC_9),        LSFT(KC_0), \
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,       KC_F6,      KC_4,           KC_5, KC_6,   KC_LBRACKET,       KC_RBRACKET, \
     KC_TRANSPARENT, KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,      KC_F12,     KC_1,           KC_2, KC_3,   LSFT(KC_LBRACKET), LSFT(KC_RBRACKET), \
     KC_NO,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, ALT_TAB,                 KC_TRANSPARENT, KC_0, KC_DOT, KC_PLUS,           KC_MINUS
-),
-[_RAISE] = LAYOUT_planck_mit(
-    KC_GRAVE,       DYN_REC_START1,  DYN_REC_START2,  KC_INSERT,      KC_HOME,        KC_PGUP,    KC_PAUSE,   LSFT(KC_7),     LSFT(KC_8),          LSFT(KC_MINUS),       LSFT(KC_EQUAL),      LSFT(KC_BSLASH), \
+  ),
+
+  [_RAISE] = LAYOUT_planck_mit(
+    KC_GRAVE,       KC_NO,           KC_NO,           KC_INSERT,      KC_HOME,        KC_PGUP,    KC_PAUSE,   LSFT(KC_7),     LSFT(KC_8),          LSFT(KC_MINUS),       LSFT(KC_EQUAL),      LSFT(KC_BSLASH), \
     KC_TRANSPARENT, DYN_MACRO_PLAY1, DYN_MACRO_PLAY2, KC_DELETE,      KC_END,         KC_PGDOWN,  KC_LEFT,    KC_DOWN,        KC_UP,               KC_RIGHT,             KC_EQUAL,            KC_BSLASH, \
     KC_TRANSPARENT, KC_MS_LEFT,      KC_MS_UP,        KC_MS_DOWN,     KC_MS_RIGHT,    KC_MS_BTN1, KC_MS_BTN2, KC_MINUS,       KC_PSCREEN,          KC_MEDIA_PREV_TRACK,  KC_MEDIA_NEXT_TRACK, KC_AUDIO_VOL_UP, \
-    DYN_REC_STOP,   KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT, KC_TRANSPARENT, KC_CAPSLOCK,            KC_TRANSPARENT, LCTL(LGUI(KC_LEFT)), LCTL(LGUI(KC_RIGHT)), KC_MEDIA_PLAY_PAUSE, KC_AUDIO_VOL_DOWN
-),
-[_ADJUST] = LAYOUT_planck_mit(
-    LALT(LCTL(KC_DELETE)), KC_NO,          KC_NO,          KC_NO,          KC_NO,         KC_NO, KC_NO, ST_MACRO_1,     KC_NO, KC_NO, ST_MACRO_0, KC_NO, \
-    KC_TRANSPARENT,        AU_ON,          AU_OFF,         AU_TOG,         ST_MACRO_2,    KC_NO, KC_NO, ST_MACRO_3,     KC_NO, KC_NO, KC_NO,      KC_NO, \
-    KC_TRANSPARENT,        MU_ON,          MU_OFF,         MU_TOG,         KC_NO,         KC_NO, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO,      RESET, \
-    KC_NO,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,KC_NO,        KC_TRANSPARENT, KC_NO, KC_NO, KC_NO,      KC_NO
-),
+    KC_NO,          KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT, KC_TRANSPARENT, KC_CAPSLOCK,            KC_TRANSPARENT, LCTL(LGUI(KC_LEFT)), LCTL(LGUI(KC_RIGHT)), KC_MEDIA_PLAY_PAUSE, KC_AUDIO_VOL_DOWN
+  ),
+
+  [_ADJUST] = LAYOUT_planck_mit(
+    LALT(LCTL(KC_DELETE)), DYN_REC_START1, DYN_REC_START2, KC_NO,          KC_NO,          KC_NO, KC_NO, MARCO_VIM_YANK, KC_NO,    KC_NO,   KC_NO,   KC_NO, \
+    KC_TRANSPARENT,        KC_NO,          KC_NO,          KC_NO,          MARCO_VIM_FIND, KC_NO, KC_NO, MARCO_VIM_PASTE,RGB_TOG,  RGB_VAI, RGB_VAD, MAGIC_TOGGLE_NKRO, \
+    KC_TRANSPARENT,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO, KC_NO, KC_NO,          RGB_MOD,  RGB_HUI, RGB_HUD, RESET, \
+    DYN_REC_STOP,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_NO,        KC_TRANSPARENT, RGB_RMOD, RGB_SAI, RGB_SAD, KC_NO
+  ),
+
+  [_FN] = LAYOUT_planck_mit(
+  KC_DOT,        KC_1,    KC_2,  KC_3,   KC_4,   KC_5,    KC_PLUS,           KC_MINUS,KC_ASTERISK,KC_SLASH,      KC_LPRN,KC_RPRN,    \
+  KC_COMMA,      KC_6,    KC_7,  KC_8,   KC_9,   KC_0,    MARCO_VIM_PREVIOUS,KC_NO,   KC_NO,      MARCO_VIM_NEXT,KC_NO,  KC_BSPACE,  \
+  KC_SCOLON,     KC_QUOTE,KC_DQT,KC_LABK,KC_RABK,KC_NO,   KC_NO,             KC_NO,   KC_NO,      KC_NO,         KC_NO,  KC_NO,      \
+  KC_TRANSPARENT,KC_NO,   KC_NO, KC_NO,  KC_NO,  KC_SPACE,                   KC_NO,   KC_NO,      KC_NO,         KC_NO,  KC_NO
+  ),
 };
 
 
@@ -77,42 +90,43 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 switch (keycode) {
-    case ST_MACRO_0:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_P))));
-
-    }
-    break;
-    case ST_MACRO_1:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LSFT(SS_TAP(X_QUOTE)) SS_DELAY(100) SS_LSFT(SS_TAP(X_EQUAL)) SS_DELAY(100) SS_TAP(X_Y));
-
-    }
-    break;
-    case ST_MACRO_2:
-    if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_V) SS_DELAY(100) SS_TAP(X_T) SS_DELAY(100) SS_TAP(X_SCOLON));
-
-    }
-    break;
-    case ST_MACRO_3:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LSFT(SS_TAP(X_QUOTE)) SS_DELAY(100) SS_LSFT(SS_TAP(X_EQUAL)) SS_DELAY(100) SS_TAP(X_P));
-
-    }
-    break;
-    case ALT_TAB:
-      if (record->event.pressed) {
-        if (!is_alt_tab_active) {
-          is_alt_tab_active = true;
-          register_code(KC_LALT);
+    case MARCO_VIM_NEXT:
+        if (record->event.pressed) {
+            SEND_STRING("gt");
         }
-        alt_tab_timer = timer_read();
-        register_code(KC_TAB);
-      } else {
-        unregister_code(KC_TAB);
-      }
-      break;
+        return false;
+    case MARCO_VIM_PREVIOUS:
+        if (record->event.pressed) {
+            SEND_STRING("gT");
+        }
+        return false;
+    case MARCO_VIM_YANK:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LSFT(SS_TAP(X_QUOTE)) SS_DELAY(100) SS_LSFT(SS_TAP(X_EQUAL)) SS_DELAY(100) SS_TAP(X_Y));
+        }
+        return false;
+    case MARCO_VIM_FIND:
+        if (record->event.pressed) {
+            SEND_STRING(SS_TAP(X_V) SS_DELAY(100) SS_TAP(X_T) SS_DELAY(100) SS_TAP(X_SCOLON));
+        }
+        return false;
+    case MARCO_VIM_PASTE:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LSFT(SS_TAP(X_QUOTE)) SS_DELAY(100) SS_LSFT(SS_TAP(X_EQUAL)) SS_DELAY(100) SS_TAP(X_P));
+        }
+        return false;
+    case ALT_TAB:
+        if (record->event.pressed) {
+            if (!is_alt_tab_active) {
+                is_alt_tab_active = true;
+                register_code(KC_LALT);
+            }
+            alt_tab_timer = timer_read();
+            register_code(KC_TAB);
+        } else {
+            unregister_code(KC_TAB);
+        }
+        return false;
 }
 return true;
 }
@@ -155,11 +169,11 @@ void encoder_update(bool clockwise) {
   }
 }
 
-#ifdef AUDIO_ENABLE
+/*#ifdef AUDIO_ENABLE
 float lead_start[][2] = SONG(MARIO_THEME);
 float lead_succeed[][2] = SONG(LEAD_SUCCESS);
 float lead_fail[][2] = SONG(LEAD_FAIL);
-#endif
+#endif*/
 LEADER_EXTERNS();
 void matrix_scan_user(void) {
 #ifdef AUDIO_ENABLE
@@ -186,7 +200,7 @@ void matrix_scan_user(void) {
       is_alt_tab_active = false;
     }
   }
-  LEADER_DICTIONARY() {
+  /*LEADER_DICTIONARY() {
     did_leader_succeed = leading = false;
 
     SEQ_ONE_KEY(KC_H) {
@@ -213,7 +227,7 @@ void matrix_scan_user(void) {
       did_leader_succeed = true;
     }
     leader_end();
-  }
+  }*/
 }
 
 bool music_mask_user(uint16_t keycode) {
@@ -226,7 +240,7 @@ bool music_mask_user(uint16_t keycode) {
   }
 }
 
-void leader_start(void) {
+/*void leader_start(void) {
 #ifdef AUDIO_ENABLE
     PLAY_SONG(lead_start);
 #endif
@@ -242,4 +256,4 @@ if (did_leader_succeed) {
     PLAY_SONG(lead_fail);
 #endif
   }
-}
+}*/
