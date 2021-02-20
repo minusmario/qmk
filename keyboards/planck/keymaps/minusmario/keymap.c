f1e11c53290fa438a75cf911580b55f3d168af79
MinusMario <tzzhmagnifica@gmail.com>
2021-02-20 15:34:32 +0800

    disable mousekey on bm40hsrgb to reduce firmware size

--- keyboards/planck/keymaps/minusmario/keymap.c
+++ keyboards/planck/keymaps/minusmario/keymap.c
@@ -44,11 +44,12 @@ enum planck_keycodes {
 // Tap-Dance
 enum {
     COMMA_DOT,
+    QUOTE_DQT
 };
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Caps Lock
     [COMMA_DOT] = ACTION_TAP_DANCE_DOUBLE(KC_COMMA, KC_DOT),
+    [QUOTE_DQT] = ACTION_TAP_DANCE_DOUBLE(KC_QUOTE, KC_DQT),
 };
 
 #define LOWER MO(_LOWER)
@@ -69,10 +70,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT_planck_mit(
-    KC_TILD,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_7,   KC_8,KC_9,         KC_LPRN,    KC_RPRN, \
-    KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_4,   KC_5,KC_6,         KC_LBRACKET,KC_RBRACKET, \
-    KC_TRNS,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_1,   KC_2,KC_3,         KC_LCBR,    KC_RCBR, \
-    KC_NO,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,        KC_TRNS,KC_0,TD(COMMA_DOT),KC_PLUS,    KC_MINUS
+    KC_TILD,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_7,   KC_8,KC_9,         KC_LPRN,      KC_RPRN, \
+    KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_4,   KC_5,KC_6,         KC_LBRACKET,  KC_RBRACKET, \
+    KC_TRNS,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_1,   KC_2,KC_3,         KC_LCBR,      KC_RCBR, \
+    KC_NO,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,        KC_TRNS,KC_0,TD(COMMA_DOT),TD(QUOTE_DQT),KC_MINUS
   ),
 
   [_RAISE] = LAYOUT_planck_mit(
