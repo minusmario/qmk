dedc292f677139d2e12bb07c98ae9109981e04a8
MinusMario <collado_ztz@hotmail.com>
2021-09-29 23:05:07 +0800

    add tapdance to bm40hsrgb add led indicator to bm40hsrgb reduce bm40hsrgb firmwaresize bug fix

--- keyboards/bm40hsrgb/keymaps/minusmario/keymap.c
+++ keyboards/bm40hsrgb/keymaps/minusmario/keymap.c
@@ -19,12 +19,14 @@ enum planck_layers {
 // Tap-Dance
 enum {
     COMMA_DOT,
-    QUOTE_DQT
+    QUOTE_DQT,
+    MINUS_PLUS
 };
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
     [COMMA_DOT] = ACTION_TAP_DANCE_DOUBLE(KC_COMMA, KC_DOT),
     [QUOTE_DQT] = ACTION_TAP_DANCE_DOUBLE(KC_QUOTE, KC_DQT),
+    [MINUS_PLUS] = ACTION_TAP_DANCE_DOUBLE(KC_MINUS, KC_PLUS)
 };
 
 #define LOWER MO(_LOWER)
@@ -44,28 +46,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_7,   KC_8,KC_9,  KC_LPRN,    KC_RPRN, \
     KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_4,   KC_5,KC_6,  KC_LBRACKET,KC_RBRACKET, \
     KC_TRNS,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_1,   KC_2,KC_3,  KC_LCBR,    KC_RCBR, \
-    KC_NO,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,        KC_TRNS,KC_0,TD(COMMA_DOT),TD(QUOTE_DQT),KC_MINUS
+    KC_NO,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,        KC_TRNS,KC_0,TD(COMMA_DOT),TD(QUOTE_DQT),TD(MINUS_PLUS)
   ),
 
   [_RAISE] = LAYOUT_planck_mit(
-    KC_GRAVE,KC_NO,          KC_NO,          KC_INSERT,KC_HOME,KC_PGUP,  KC_PAUSE,KC_AMPR, KC_ASTR,            KC_UNDS,             KC_PLUS,            KC_PIPE, \
-    KC_TRNS, DYN_MACRO_PLAY1,DYN_MACRO_PLAY2,KC_DELETE,KC_END, KC_PGDOWN,KC_LEFT, KC_DOWN, KC_UP,              KC_RIGHT,            KC_EQUAL,           KC_BSLASH, \
-    KC_TRNS, KC_NLCK,        KC_CAPS,        KC_SLCK,    KC_NO,  KC_NO,    KC_NO,   KC_MINUS,KC_PSCREEN,         KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,KC_AUDIO_VOL_UP, \
-    KC_NO,   KC_TRNS,        KC_TRNS,        KC_TRNS,  KC_TRNS,KC_TRNS,           KC_TRNS, LCTL(LGUI(KC_LEFT)),LCTL(LGUI(KC_RIGHT)),KC_MEDIA_PLAY_PAUSE,KC_AUDIO_VOL_DOWN
+    KC_GRAVE,KC_PAUSE,           KC_PSCREEN,          KC_INSERT,KC_HOME,KC_PGUP,  LGUI(KC_D),  KC_AMPR, KC_ASTR,            KC_UNDS,             KC_PLUS,     KC_PIPE, \
+    KC_TRNS, LCTL(LGUI(KC_LEFT)),LCTL(LGUI(KC_RIGHT)),KC_DELETE,KC_END, KC_PGDOWN,KC_LEFT,KC_DOWN, KC_UP,              KC_RIGHT,            KC_EQUAL,    KC_BSLASH, \
+    KC_TRNS, KC_NLCK,            KC_CAPS,             KC_SLCK,  KC_NO,  KC_NO,    KC_NO,  KC_MINUS,KC_NO,              KC_MEDIA_PLAY_PAUSE, KC_MUTE,     KC_NO, \
+    KC_NO,   KC_TRNS,            KC_TRNS,             KC_TRNS,  KC_TRNS,    KC_TRNS,      KC_TRNS, KC_MEDIA_PREV_TRACK,KC_AUDIO_VOL_UP,KC_AUDIO_VOL_DOWN,KC_MEDIA_NEXT_TRACK
   ),
 
   [_ADJUST] = LAYOUT_planck_mit(
-    LALT(LCTL(KC_DELETE)),DYN_REC_START1,DYN_REC_START2,KC_NO,  KC_NO,         KC_NO,KC_NO,MARCO_VIM_YANK, KC_NO,   RGB_SPI,RGB_SPD,KC_NO,             \
-    KC_TRNS,              KC_NLCK,       KC_SLCK,       KC_NO,  MARCO_VIM_FIND,KC_NO,KC_NO,MARCO_VIM_PASTE,RGB_TOG, RGB_VAI,RGB_VAD,MAGIC_TOGGLE_NKRO, \
+    LALT(LCTL(KC_DELETE)),DYN_REC_START1,DYN_REC_START2,  KC_NO,  KC_NO,         KC_NO,KC_NO,MARCO_VIM_YANK, KC_NO,   RGB_SPI,RGB_SPD,KC_NO,             \
+    KC_TRNS,              DYN_MACRO_PLAY1,DYN_MACRO_PLAY2,KC_NO,  MARCO_VIM_FIND,KC_NO,KC_NO,MARCO_VIM_PASTE,RGB_TOG, RGB_VAI,RGB_VAD,MAGIC_TOGGLE_NKRO, \
     KC_TRNS,              KC_NO,         KC_NO,         KC_NO,  KC_NO,         KC_NO,KC_NO,KC_NO,          RGB_MOD, RGB_HUI,RGB_HUD,RESET, \
-    DYN_REC_STOP,         KC_TRNS,       KC_TRNS,       KC_TRNS,KC_TRNS,       KC_NO,      KC_TRNS,        RGB_RMOD,RGB_SAI,RGB_SAD,KC_NO
+    DYN_REC_STOP,         KC_TRNS,       KC_TRNS,       KC_TRNS,KC_TRNS,       KC_NO,      KC_TRNS,        RGB_RMOD,RGB_SAI,RGB_SAD,EEPROM_RESET
   ),
 
   [_FN] = LAYOUT_planck_mit(
   KC_TRNS,KC_7,   KC_8, KC_9,   KC_LPRN, KC_RPRN,KC_PLUS,           KC_MINUS,KC_ASTR,KC_SLASH,      KC_NO,  KC_TRNS,\
   KC_TRNS,KC_4,   KC_5, KC_6,   KC_QUOTE,KC_DQT, MARCO_VIM_PREVIOUS,KC_NO,   KC_NO,  MARCO_VIM_NEXT,KC_NO,  KC_NO,  \
   KC_TRNS,KC_1,   KC_2, KC_3,   KC_COMMA,KC_DOT, KC_LT,             KC_GT,   KC_NO,  KC_NO,         KC_NO,  KC_TRNS,\
-  KC_TRNS,KC_TRNS,KC_0, KC_TRNS,KC_NO,   KC_TRNS,                   KC_NO,   KC_TRNS,KC_TRNS,       KC_TRNS,KC_TRNS
+  KC_TRNS,KC_SPACE,KC_0, KC_ENTER,KC_NO,   KC_TRNS,                   KC_NO,   KC_TRNS,KC_TRNS,       KC_TRNS,KC_TRNS
   ),
 };
 
@@ -84,24 +86,24 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
         },
 
     [2] = {
-        {85,203,158}, {0,0,0}, {0,0,0}, {32,255,234}, {32,255,234}, {31,255,255}, {85,203,158}, {0,204,255}, {0,204,255}, {0,204,255}, {0,204,255}, {0,204,255},
+        {85,203,158}, {31,255,255}, {31,255,255}, {32,255,255}, {32,255,255}, {31,255,255}, {105,255,255}, {0,204,255}, {0,204,255}, {0,204,255}, {0,204,255}, {0,204,255},
         {0,0,255}, {141,255,233}, {141,255,233}, {31,255,255}, {31,255,255}, {31,255,255}, {0,0,255}, {0,0,255}, {0,0,255}, {0,0,255}, {141,255,233}, {141,255,233},
-        {0,0,255}, {0,255,255}, {60,255,255}, {240,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,204,255}, {31,255,255}, {205,255,255}, {205,255,255}, {14,222,242},
-        {0,0,0}, {0,0,255}, {0,0,255}, {0,0,255}, {105,255,255}, {14,255,255}, {105,255,255}, {0,0,255}, {0,0,255}, {85,203,158}, {14,222,242}
+        {0,0,255}, {0,255,255}, {60,255,255}, {240,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,204,255}, {0,0,0}, {205,255,255}, {85,203,158}, {0,0,0},
+        {0,0,0}, {0,0,255}, {0,0,255}, {0,0,255}, {105,255,255}, {14,255,255}, {105,255,255}, {0,0,255}, {14,222,242}, {14,222,242}, {0,0,255}
         },
 
     [3] = {
         {0,183,238}, {134,255,213}, {134,255,213}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {105,255,255}, {0,0,0}, {233,218,217}, {233,218,217}, {0,0,0},
         {0,0,255}, {14,255,255}, {14,255,255}, {0,0,0}, {105,255,255}, {0,0,0}, {0,0,0}, {105,255,255}, {120,255,255}, {14,222,242}, {14,255,255}, {0,255,255},
         {0,0,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {60,255,255}, {154,86,255}, {154,86,255}, {243,222,234},
-        {243,222,234}, {0,0,255}, {0,0,255}, {0,0,255}, {105,255,255}, {0,0,0}, {105,255,255}, {60,255,255}, {85,203,158}, {85,203,158}, {0,0,0}
+        {243,222,234}, {0,0,255}, {0,0,255}, {0,0,255}, {105,255,255}, {0,0,0}, {105,255,255}, {60,255,255}, {85,203,158}, {85,203,158}, {0,255,255}
         },
 
     [4] = {
         {0,0,255}, {141,255,233}, {141,255,233}, {141,255,233}, {33,255,255}, {33,255,255}, {50,153,244}, {50,153,244}, {50,153,244}, {50,153,244}, {0,0,0}, {0,0,255},
         {0,0,255}, {141,255,233}, {141,255,233}, {141,255,233}, {33,255,255}, {33,255,255}, {205,255,255}, {0,0,0}, {0,0,0}, {205,255,255}, {0,0,0}, {0,0,0},
         {0,0,255}, {141,255,233}, {141,255,233}, {141,255,233}, {33,255,255}, {33,255,255}, {33,255,255}, {33,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,255},
-        {14,255,255}, {0,0,255}, {141,255,233}, {0,0,255}, {0,0,0}, {250,159,255}, {0,0,0}, {0,0,255}, {0,0,255}, {0,0,255}, {0,0,255}
+        {14,255,255}, {249,228,255}, {141,255,233}, {249,228,255}, {0,0,0}, {250,159,255}, {0,0,0}, {0,0,255}, {0,0,255}, {0,0,255}, {0,0,255}
         },
 };
 
